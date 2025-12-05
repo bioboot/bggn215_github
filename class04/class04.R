250e168a7506a3826b55fb4a08b44205b5fb83fb
Barry Grant <bjgrant@ucsd.edu>
2025-12-05 14:36:08 -0800

    Add some more classes

---
+++ class04/class04.R
@@ -0,0 +1,15 @@
+#' ---
+#' title: "Class 4 Lab"
+#' author: "Barry (PID 911)"
+#' ---
+
+# This is my first R script
+x <- 1:50
+plot(x)
+plot(x, col="blue")
+plot(x, sin(x), typ="l", col="red", lwd=3)
+
+log(10)
+log(10, base=10)
+log(10,2)
+
